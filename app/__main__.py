2fe5b75379545fde2ecede68edd46db0fe7a06e2
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-09-19 10:10:16 +0900

    株価取得部分を別モジュールに移動

--- app/__main__.py
+++ app/__main__.py
@@ -1,49 +1,16 @@
 from datetime import datetime as dt
-import sys
 import os
-from dotenv import load_dotenv
-from yahoo_finance_api2 import share
-from yahoo_finance_api2.exceptions import YahooFinanceError
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
 
-load_dotenv()
 line_bot_api = LineBotApi(os.getenv("LINEBOT_ACCESS_TOKEN"))
-
-
-def get_latest_close():
-    """ 
-    最も最近の指数(単位時間の終値)のみ取得する  
-    @return {timestamp: int, close: float}
-    """
-    my_share = share.Share(os.getenv("TARGET_STOCK_NAME"))
-    symbol_data = None
-    try:
-        symbol_data = my_share.get_historical(share.PERIOD_TYPE_DAY,
-                                              1,
-                                              share.FREQUENCY_TYPE_MINUTE,
-                                              1)
-    except YahooFinanceError as e:
-        print(e.message)
-        sys.exit(1)
-
-    if symbol_data is None:
-        return None
-
-    timestamps = symbol_data.get("timestamp", [])
-    closes = symbol_data.get("close", [])
-    for i in reversed(range(len(timestamps))):
-        if closes is not None:
-            return {"timestamp": timestamps[i], "close": closes[i]}
-    return None
-
 
 def send_line_bot(s):
     line_bot_api.broadcast(TextSendMessage(text=s))
 
-
 if __name__ == "__main__":
+    from .get_value.main import get_latest_close
     latest = get_latest_close()
     if latest is not None:
         print(latest)
---
+++ app/get_value/main.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from yahoo_finance_api2 import share
+from yahoo_finance_api2.exceptions import YahooFinanceError
+def get_latest_close():
+    """ 
+    最も最近の指数(単位時間の終値)のみ取得する  
+    @return {timestamp: int, close: float}
+    """
+    my_share = share.Share(os.getenv("TARGET_STOCK_NAME"))
+    symbol_data = None
+    try:
+        symbol_data = my_share.get_historical(share.PERIOD_TYPE_DAY,
+                                              2,
+                                              share.FREQUENCY_TYPE_MINUTE,
+                                              1)
+    except YahooFinanceError as e:
+        print(e.message)
+        sys.exit(1)
+
+    if symbol_data is None:
+        return None
+
+    timestamps = symbol_data.get("timestamp", [])
+    closes = symbol_data.get("close", [])
+    for i in reversed(range(len(timestamps))):
+        if closes is not None:
+            return {"timestamp": timestamps[i], "close": closes[i]}
+    return None
