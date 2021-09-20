129f55b57ec39e2d5287a924174b240b0fd997ef
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-09-20 19:16:50 +0900

    #4 デバッグモード追加

--- README.md
+++ README.md
@@ -1,5 +1,43 @@
 # NikkeiAveToLine
+
 日経平均をlineに通知する。（あとで汎用的にする）
+
+## 準備
+
+### 環境変数
+
+ローカル環境なら`.env`、パブリック環境ならsecretの仕組みを活用する。
+
+#### TARGET_STOCK_NAME
+
+必須。  
+`yahoo_finance_api2`で使用する株式コードを記載する。  
+日経平均の場合は`^N225`
+
+#### LINEBOT_ACCESS_TOKEN
+
+必須。  
+`line developer`で取得したボットチャネルのアクセストークン。  
+
+#### DEBUG
+
+任意。ローカル環境では設定推奨。  
+値はTruesyであれば何でもよい。  
+このフラグがONの場合デバッグモードになる。lineの通知が自分宛てのみになったりする。
+
+#### MY_LINE_ID
+
+任意、`DEBUG`設定時は必須。  
+自分のLINEのユーザーIDを設定しておくと自分にだけ通知が飛ぶので良き。  
+
+#### .envの設定例
+
+```
+TARGET_STOCK_NAME=^N225
+LINEBOT_ACCESS_TOKEN=/XXXXXXX/....(結構長い)
+DEBUG=true
+MY_LINE_ID=U123456789XXXXXX
+```
 
 ## 実行
 
--- app/send_notification/main.py
+++ app/send_notification/main.py
@@ -4,9 +4,20 @@ from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
 
+
 def send_notification(timestamp: dt, value: float):
     send_line_bot(timestamp.strftime('%Y/%m/%d %H:%M') + "ごろ: " + str(value))
 
+
 def send_line_bot(s):
+    notifi_func = get_notification_function()
+    notifi_func(TextSendMessage(text=s))
+
+
+def get_notification_function():
     line_bot_api = LineBotApi(os.getenv("LINEBOT_ACCESS_TOKEN"))
-    line_bot_api.broadcast(TextSendMessage(text=s))
+    if (os.getenv("DEBUG") is None):
+        return line_bot_api.broadcast
+    else:
+        print("debug")
+        return lambda mes: line_bot_api.push_message(os.getenv("MY_LINE_ID"), mes)
