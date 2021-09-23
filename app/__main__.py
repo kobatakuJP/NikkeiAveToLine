70ecf07e8e58a20808a8430b46db27ea511bdc35
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-09-23 16:18:04 +0900

    is_todayを追加

--- app/__main__.py
+++ app/__main__.py
@@ -3,12 +3,18 @@ from datetime import timezone as tz
 from datetime import timedelta as td
 
 if __name__ == "__main__":
+    def is_today(datetime):
+        return datetime.strftime('%Y%m%d') == dt.now(tz(td(hours=9))).strftime('%Y%m%d')
     from .get_value.main import get_latest_close
     latest = get_latest_close()
     if latest is not None:
         print(latest)
-        from .send_notification.main import send_notification
-        send_notification(dt.fromtimestamp(
-            latest["timestamp"]/1000, tz=tz(td(hours=9))), latest["close"])
+        datetime = dt.fromtimestamp(
+            latest["timestamp"]/1000, tz=tz(td(hours=9)))
+        if is_today(datetime):
+            from .send_notification.main import send_notification
+            send_notification(datetime, latest["close"])
+        else:
+            print("not today")
     else:
         print("no data")
